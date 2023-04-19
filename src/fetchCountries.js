b88a63cea3dfaaeef5ce11b65e8b77c2263bea91
dimside <dimside29@gmail.com>
2023-04-20 00:26:57 +0300

    deleted-notiflix-from-fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,3 @@
-import Notiflix from 'notiflix';
-
 export const fetchCountries = function (name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
